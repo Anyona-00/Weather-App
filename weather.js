36b9ca4457131296a2d59afd124f3669b7998185
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2024-01-16 19:37:39 +0300

    bug

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
-import axios from "axios"; /*fetching liablary*/
+import axios from "axios"; /*fetching liblary*/
 
 //https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime
 export function getWeather(lat, lon, timezone){
-    axios.get("https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime",{
+    return axios.get("https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime",{
     params:{
         latitude: lat,
         longitude: lon,
